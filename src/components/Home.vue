eba469361ebcf55e993841e9c8e6e0ac7c7c68f4
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2024-04-07 18:09:30 +0000

    fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,8 +5,8 @@ const playing = ref(false)
 const current = ref(null)
 const input = ref('')
 const videos = ref([])
+const error = ref('')
 async function change() {
-    console.log(input.value)
     const data = await fetch(`https://weeb-api.vercel.app/ytsearch?query=${input.value}`)
     .then(res => res.json())
     if(Array.isArray(data)) {
@@ -14,18 +14,26 @@ async function change() {
     }
 }
 
-function toggle(url) {
+async function toggle(url) {
     if (playing.value && current.value === url) {
         audio.pause()
         playing.value = false
+        console.log('paused')
     }else if(current.value === url && playing.value === false) {
         audio.play()
         playing.value = true
+        console.log('playing')
     }else {
         audio.src = `https://weeb-api.vercel.app/ytaudio?query=${url}`
-        audio.play()
+        try {
+        await audio.play()
         playing.value = true
         current.value = url
+        console.log('start')
+        }catch(e) {
+        console.log(e)
+        error.value = 'An error occured'
+        }
     }
 }
 </script>
@@ -43,18 +51,16 @@ function toggle(url) {
                         <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
                       </svg>
                     </div>
-                    <input type="text" v-model="input" @change="change" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" required="">
-                  </div>
+                    <input type="text" v-model="input" @change="change" class="block w-full p-2 pl-10 text-sm  border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500" placeholder="Search" required="">
+                </div>
                 </div>
               </div>
               <div class="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
-                <button type="button" class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
-                  <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
-                  </svg>
+                <button type="button" @click="change" class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-primary-800">
                   Search
                 </button>
                </div>
+               <p class="flex flex-row text-center text-white">{{ error }}</p>
             </div>
           </div>
         </div>
